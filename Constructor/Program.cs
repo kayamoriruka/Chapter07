17ac81046fa92b7e03422054da47072ccf502353
kayamoriruka <kayamoriruka@hotmail.com>
2023-03-28 15:41:23 +0900

    0328

---
+++ Constructor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Constructor
+{
+    class Cat
+    {
+        public Cat()
+        {
+            Name = "";
+            Color = "";
+        }
+        ~Cat()
+        {
+            Console.WriteLine($"{Name} : bye");
+        }
+        public Cat(string _Name, string _Color)
+        {
+            Name = _Name;
+            Color = _Color;
+        }
+
+        public string Name;
+        public string Color;
+
+        public void Meow()
+        {
+            Console.WriteLine($"{Name} : 야옹");
+        }
+    }
+        class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Cat kitty = new Cat("키티", "하얀색");
+            kitty.Meow();
+            Console.WriteLine($"{kitty.Name} : {kitty.Color}");
+
+            Cat nero = new Cat("네로", "검은색");
+            nero.Meow();
+            Console.WriteLine($"{nero.Name} : {kitty.Color}");
+        }
+    }
+}
