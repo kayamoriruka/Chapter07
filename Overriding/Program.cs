040a3ba1b42b9f9e4b01d320716af4fe4259883d
kayamoriruka <kayamoriruka@hotmail.com>
2023-03-30 15:41:46 +0900

    0330

---
+++ Overriding/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Overriding
+{
+    class Program
+    {
+        class ArmorSuite
+        {
+            public virtual void Initialize()
+            {
+                Console.WriteLine("Armored");
+            }
+        }
+
+        class IronMan : ArmorSuite
+        {
+            public override void Initialize()
+            {
+                base.Initialize();
+                Console.WriteLine("Repulsor Rays Armed");
+            }
+        }
+
+        class WarMachine : ArmorSuite
+        {
+            public override void Initialize()
+            {
+                base.Initialize();
+                Console.WriteLine("Double-Barrel Cannons Armed");
+                Console.WriteLine("Micro-Rocket Launcher Armed");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            ArmorSuite armorSuite = new ArmorSuite();
+            armorSuite.Initialize();
+
+            Console.WriteLine("");
+            ArmorSuite ironMan = new IronMan();
+            ironMan.Initialize();
+
+            Console.WriteLine("");
+            ArmorSuite warMachine = new WarMachine();
+            warMachine.Initialize();
+        }
+    }
+}
